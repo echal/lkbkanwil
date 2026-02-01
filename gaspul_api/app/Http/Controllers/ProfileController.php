5012919b1d721778c4b02c44ca9ef995643e135e
prakomkemenagsulbar <prakomkemenagsulbar@gmail.com>
2026-02-02 01:18:02 +0800

    fix: remove undefined 'unit' relation load in ProfileController

--- gaspul_api/app/Http/Controllers/ProfileController.php
+++ gaspul_api/app/Http/Controllers/ProfileController.php
@@ -20,7 +20,7 @@ class ProfileController extends Controller
     public function edit()
     {
         $user = Auth::user();
-        $user->load(['unitKerja', 'unit']);
+        $user->load('unitKerja');
 
         return view('profile.edit', [
             'user' => $user,
